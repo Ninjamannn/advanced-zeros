558c8cb68f24a9f35875e6287dd0b0f662f60444
ninjamannn <a.logatski@gmail.com>
2018-03-15 18:58:24 +0300

    work

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
-module.exports = function getZerosCount(number, base) {
-  // your implementation
+function factorize(num) {
+    let stack = [];
+    let ml = 2;
+    while (num !== 1) {
+        if (ml === num) {
+            stack.push(num);
+            return stack;
+        } else if (num % ml === 0) {
+            stack.push(ml);
+            num = num / ml;
+        } else {
+            ml++;
+        }
+    }
+    return stack;
 }
+
+function divideByExponent(number, exp) {
+    let zerosCount = 0;
+    while (number) {
+        number = Math.floor(number/exp);
+        zerosCount += number;
+    }
+    return zerosCount;
+}
+
+module.exports = function getZerosCount(number, base) {
+    const multipliers = factorize(base);
+    let last;
+    let current;
+    for (let val of multipliers){
+        let degree = multipliers.filter(x => x === val).length;
+        last = (divideByExponent(number, val)/degree);
+        if (last < current || current === undefined){
+            current = Math.floor(last);
+        }
+    }
+    return current;
+};
